32b9767f2dbc9c5cf9e0f0de8627cea3e42250f8
O-K11 <81224638+O-K11@users.noreply.github.com>
2021-03-23 14:23:10 +0300

    Add files via upload

---
+++ sistem2.py
@@ -0,0 +1,41 @@
+ozkan = 1235
+kurtarma=1234
+
+
+sıfre=int(input("Kullanıcı şifrenizi girin="))
+
+if sıfre==ozkan:
+      print("Hoşgeldiniz sayın cumhurbaşkanım")
+      import time
+      time.sleep(1)
+      import sys
+      sys.exit(1)
+else:
+      print("Tekrar deneyin")
+      
+deneme=int(input("Kurtarma şifrenizi  girin="))
+if deneme == kurtarma:
+      
+      print("Hoşgeldiniz sayın cumhurbaşkanım")
+
+else:
+      print("Girişiniz engellendi")
+      
+
+      
+import time
+time.sleep(1.9)
+
+
+
+
+
+
+
+    
+
+
+
+
+              
+   
